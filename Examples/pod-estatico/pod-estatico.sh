d107b06fa65248bf3b098066aa93e60de3a5aeee
Leonardo Viana <leonardoviana00@hotmail.com>
2023-10-02 07:31:07 -0300

    feature(pod-static): example pod estatico

---
+++ Examples/pod-estatico/pod-estatico.sh
@@ -0,0 +1,17 @@
+mkdir -p /etc/kubernetes/manifests/
+cat <<EOF >/etc/kubernetes/manifests/static-web.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: static-web
+  labels:
+    role: myrole
+spec:
+  containers:
+    - name: web
+      image: nginx
+      ports:
+        - name: web
+          containerPort: 80
+          protocol: TCP
+EOF
